8c5a8e06be7eac409a0ab2b26f41e5194cf69b06
LQ1234 <larryqiu424@yahoo.com>
2018-11-18 21:27:07 -0500

    ads

--- defrun.js
+++ defrun.js
@@ -1,86 +1,112 @@
-window.onload = function(){
-fetch('https://larrys.tech/files.json')
-.then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    createHeaders((json))
+window.onload = function() {
+  fetch('https://larrys.tech/files.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      createHeaders((json))
 
-  });
+    });
 
-function createHeaders(obj) {
+  function createHeaders(obj) {
 
-  var header = document.createElement("div");
-  header.id = "header";
-  var name = document.createElement("div");
-  name.id = "name";
-  name.innerHTML = "Larrys<span>.</span>Tech";
-  header.appendChild(name);
+    var header = document.createElement("div");
+    header.id = "header";
+    var name = document.createElement("div");
+    name.id = "name";
+    name.innerHTML = "Larrys<span>.</span>Tech";
+    header.appendChild(name);
 
-  var subname = document.createElement("div");
-  subname.id = "subname";
-  subname.innerHTML = "My code and other stuff";
-  header.appendChild(subname);
+    var subname = document.createElement("div");
+    subname.id = "subname";
+    subname.innerHTML = "My code and other stuff";
+    header.appendChild(subname);
 
 
-  var headermenu = document.createElement("div");
-  headermenu.id = "header-menu";
-  header.appendChild(headermenu);
-  var list = document.createElement("ul");
-  headermenu.appendChild(list);
+    var headermenu = document.createElement("div");
+    headermenu.id = "header-menu";
+    header.appendChild(headermenu);
+    var list = document.createElement("ul");
+    headermenu.appendChild(list);
 
-  var ld = Object.keys(obj);
-  for (var i = 0; i < ld.length; i++) {
-    var tn = ld[i];
-    if (typeof obj[tn] === "string") {
-      var nw = document.createElement("a");
-      nw.href = obj[tn];
-      var nwc = document.createElement("li");
-      nwc.innerHTML = tn;
-      nw.appendChild(nwc);
-      list.appendChild(nw);
-    } else {
-      var nwc = document.createElement("li");
-      list.appendChild(nwc);
-      nwc.className = "dropdown";
-      nwc.innerHTML += tn;
-      nwc.innerHTML += "<span class=texticon>&#8801;</span>";
+    var ld = Object.keys(obj);
+    for (var i = 0; i < ld.length; i++) {
+      var tn = ld[i];
+      if (typeof obj[tn] === "string") {
+        var nw = document.createElement("a");
+        nw.href = obj[tn];
+        var nwc = document.createElement("li");
+        nwc.innerHTML = tn;
+        nw.appendChild(nwc);
+        list.appendChild(nw);
+      } else {
+        var nwc = document.createElement("li");
+        list.appendChild(nwc);
+        nwc.className = "dropdown";
+        nwc.innerHTML += tn;
+        nwc.innerHTML += "<span class=texticon>&#8801;</span>";
 
-      var ddi = document.createElement("div");
-      ddi.className = "dropdown-items"
-      nwc.appendChild(ddi);
-      var ldi = Object.keys(obj[tn]);
-      for (var p = 0; p < ldi.length; p++) {
-        var nwi = document.createElement("a");
-        nwi.innerHTML = ldi[p];
-        nwi.href = obj[tn][ldi[p]];
-        ddi.appendChild(nwi);
+        var ddi = document.createElement("div");
+        ddi.className = "dropdown-items"
+        nwc.appendChild(ddi);
+        var ldi = Object.keys(obj[tn]);
+        for (var p = 0; p < ldi.length; p++) {
+          var nwi = document.createElement("a");
+          nwi.innerHTML = ldi[p];
+          nwi.href = obj[tn][ldi[p]];
+          ddi.appendChild(nwi);
+        }
       }
     }
+    document.body.insertBefore(header, document.body.childNodes[0]);
+
+    var spacefiller = document.createElement("div");
+    spacefiller.id = "spacefiller";
+    document.body.insertBefore(spacefiller, document.body.childNodes[1]);
+    var src = document.createElement("script");
+    src.src = "https://code.jquery.com/jquery-3.3.1.slim.min.js";
+    head.insertBefore(src, head.childNodes[0]);
+
+    src.onload = function() {
+
+      var n = function() {
+        $("#spacefiller")[0].style.height = ($("#header").height() + 15) + "px";
+      }
+      $(window).resize(n);
+      n();
+    };
   }
-  document.body.insertBefore(header, document.body.childNodes[0]);
-
-
-}
 
 
 
-var head = document.head;
-var link = document.createElement("link");
-link.type = "text/css";
-link.rel = "stylesheet";
-link.href = "defcss.css";
-head.insertBefore(link,head.childNodes[0]);
+  var head = document.head;
+  var defcss = document.createElement("link");
+  defcss.type = "text/css";
+  defcss.rel = "stylesheet";
+  defcss.href = "defcss.css";
+  head.insertBefore(defcss, head.childNodes[0]);
 
-var link2 = document.createElement("link");
-link2.type = "text/css";
-link2.rel = "stylesheet";
-link2.href = "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css";
-head.insertBefore(link2,head.childNodes[0]);
+  defcss.onload = function() {
+    var bMobile =
+      navigator.userAgent.indexOf("Mobile") !== -1 ||
+      navigator.userAgent.indexOf("iPhone") !== -1 ||
+      navigator.userAgent.indexOf("Android") !== -1 ||
+      navigator.userAgent.indexOf("Windows Phone") !== -1;
+    console.log("ismoblie:" + bMobile);
+    if (bMobile) {
+      var head = document.head;
+      var link = document.createElement("link");
+      link.type = "text/css";
+      link.rel = "stylesheet";
+      link.href = "defcssmobile.css";
+      head.insertBefore(link, defcss.nextSibling);
+    }
+  }
+  var link2 = document.createElement("link");
+  link2.type = "text/css";
+  link2.rel = "stylesheet";
+  link2.href = "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css";
+  head.insertBefore(link2, head.childNodes[0]);
 
-var src = document.createElement("script");
-src.src = "https://code.jquery.com/jquery-3.3.1.slim.min.js";
-head.insertBefore(src,head.childNodes[0]);
 
-
-} ;
+};
