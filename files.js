2d4dbc1adefdacac0627573a52f78fee077a8596
LQ1234 <larryqiu424@yahoo.com>
2019-06-02 11:27:48 -0400

    Shaders

--- files.js
+++ files.js
@@ -4,11 +4,13 @@ var files={
    "Autoencoder":"autoencoder.html",
    "Particle system":"Particle%20system%20with%20UI/index.html",
    "Multiplayer game":"io.html",
+
    "Other":{
       "Autoclicker":"autoclicker.html",
       "Math Library":"math.html",
       "Password Generator":"passwordgen.html",
-      "Gravity particle simulation":"worldsimulator.html"
+      "Gravity particle simulation":"worldsimulator.html",
+      "Shader Comparison":"mc%20video/index.html"
 
    }
 }
