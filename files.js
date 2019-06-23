476e2599e10ac07014db89946927061ccaf72e35
LQ1234 <larryqiu424@yahoo.com>
2019-06-23 17:43:02 -0400

    Line Editor

--- files.js
+++ files.js
@@ -2,15 +2,16 @@ var files={
   "Home":"index.html",
 
    "Autoencoder":"autoencoder.html",
-   "Particle system":"Particle%20system%20with%20UI/index.html",
    "Multiplayer game":"io.html",
-
+   "Particle Simulations":{
+     "Gravity particle simulation":"worldsimulator.html",
+     "Particle system":"Particle%20system%20with%20UI/index.html"
+   },
    "Other":{
       "Autoclicker":"autoclicker.html",
       "Math Library":"math.html",
       "Password Generator":"passwordgen.html",
-      "Gravity particle simulation":"worldsimulator.html",
-      "Shader Comparison":"mc%20video/index.html"
-
+      "Shader Comparison":"mc%20video/index.html",
+      "Line Editor":"lineEditor.html"
    }
 }
