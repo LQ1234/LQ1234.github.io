d8fde6348f38f15cf5cdf367025cda07486335c4
LQ1234 <larryqiu424@yahoo.com>
2019-06-30 22:31:04 -0400

    Creation times

--- files.js
+++ files.js
@@ -1,72 +1,72 @@
 var files = {
   "Home":{
     "tags": [],
-    "time": "now",
+    "time": "Jun 23, 2018",
     "link": "index.html"
   },
   "Autoencoder": {
     "tags": ["ML"],
-    "time": "",
+    "time": "Nov 14, 2018",
     "link": "autoencoder.html"
   },
   "Multiplayer game": {
     "tags": ["Socket.io"],
-    "time": "",
+    "time": "Dec 9, 2018",
     "link": "io.html"
   },
   "Particle Simulations": {
     "Gravity particle simulation": {
       "tags": ["Simulation"],
-      "time": "",
+      "time": "May 12, 2019",
       "link": "worldsimulator.html"
     },
     "Particle system": {
       "tags": ["Simulation","Tools"],
-      "time": "",
+      "time": "May 26, 2019",
       "link": "Particle%20system%20with%20UI/index.html"
     },
   },
   "WebGL Stuff": {
     "Wavy": {
       "tags": ["WebGL","WIP"],
-      "time": "",
+      "time": "Jun 9, 2019",
       "link": "WIP/WGPM/index.html"
     },
     "Equation Particle Simulator": {
       "tags": ["WebGL","Simulation"],
-      "time": "",
+      "time": "Jun 23, 2019",
       "link": "Equation%20Particle%20Simulator/index.html"
     },
     "WebGl Predetermined Particles": {
       "tags": ["WebGL","Simulation"],
-      "time": "",
+      "time": "Jun 30, 2019",
       "link": "WebGl%20Predetermined%20Particles/index.html"
     },
   },
   "Other": {
     "Autoclicker": {
       "tags": ["Tools"],
-      "time": "",
+      "time": "Nov 14, 2018",
       "link": "autoclicker.html"
     },
     "Math Library 2.1": {
       "tags": ["Lib"],
-      "time": "",
+      "time": "Apr 19, 2019",
       "link": "math.html"
     },
     "Password Generator": {
       "tags": ["Tools"],
-      "time": "",
+      "time": "Nov 14, 2018",
       "link": "passwordgen.html"
     },
     "Shader Comparison": {
       "tags": ["Minecraft"],
-      "time": "",
+      "time": "Jun 1, 2019",
       "link": "mc%20video/index.html"
     },
     "Line Editor": {
       "tags": ["Tools"],
-      "time": "",
+      "time": "Jun 23, 2019",
       "link": "lineEditor.html"
     },
   },
